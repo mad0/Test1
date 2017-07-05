e1bbacd0679d8cc62acc41e4145fc609a2fbb805
mad <mad@DESKTOP-P27D63S>
2017-07-05 23:16:14 +0200

    Add project files.

---
+++ Test1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class Creature {
+	protected:
+		std::string _name;
+		std::string _class;
+		std::string _weapon;
+		int _hp;
+		int _damage;
+		int _armor;
+	public:
+		Creature() { std::cout << "CREATURE CONSTR\n"; }
+		virtual ~Creature() = 0 { std::cout << "CREATURE DESTR\n"; }
+};
+
+class Player : public Creature {
+	public:
+		Player() { std::cout << "PLAYER CONSTR\n"; }
+		~Player() { std::cout << "PLAYER DETR\n"; }
+};
+
+
+int main() {
+	Player *wsk = new Player;
+	delete wsk;
+	return 0;
+}
